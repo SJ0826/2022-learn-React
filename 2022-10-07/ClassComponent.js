306f49145b5c0caa5a5c10d52a2ce2ef25e13d29
sujin <ikosdu60@gmail.com>
2022-10-07 13:48:25 +0900

    [ADD] React: 패스트캠퍼스 for velopert 클래스형 컴포넌트

---
+++ 2022-10-07/ClassComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  }
+  render() {
+    const { color, isSpecial, name} = this.props;
+    return (
+      <div style={ color }>
+        {isSpecial && <b>*</b>}
+        안녕하세요 
+      </div>
+    )
+  }
+}

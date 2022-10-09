15729651dd4fab1550da4b409888bffb38b98a02
sujin <ikosdu60@gmail.com>
2022-10-09 20:13:41 +0900

    [ADD] React: 패스트캠퍼스 for velopert Styled-components

---
+++ 2022-10-09/styling-with-styled-components/Button.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-width: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  align-items: center;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: #339af0
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>
+}
+
+export default Button;

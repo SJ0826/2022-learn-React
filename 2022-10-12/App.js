fe3b9082ba8f81fb840855a162ed4cf621574e9e
sujin <ikosdu60@gmail.com>
2022-10-12 20:23:13 +0900

    [ADD] React: 패스트캠퍼스 for velopert React-Router

---
+++ 2022-10-12/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Link, Routes } from 'react-router-dom';
+import About from './About';
+import HistorySample from './HistorySample';
+import Home from './Home';
+import Profiles from './Profiles';
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>     
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles" element={<Profiles />} />
+        <Route path='/history' element={<HistorySample />} />
+      </Routes>
+ 
+    </div>
+  );
+};
+
+export default App;

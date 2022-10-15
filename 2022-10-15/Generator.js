70304de16b159cec07acd1c879b29bb6b09a7285
sujin <ikosdu60@gmail.com>
2022-10-15 17:32:27 +0900

    [ADD] React: 패스트캠퍼스 for velopert Generator문법

---
+++ 2022-10-15/Generator.js
@@ -0,0 +1,8 @@
+function* sumGenerator() {
+    console.log('sumGenerator이 시작됐습니다.');
+    let a = yield;
+    console.log('a값을 받았습니다.');
+    let b = yield;
+    console.log('b값을 받았습니다.');
+    yield a + b;
+}
